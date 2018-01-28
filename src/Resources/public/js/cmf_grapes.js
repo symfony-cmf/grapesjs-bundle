28e1ba8d06126656a56b7240a607b766290b85e7
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-01-28 09:24:35 +0100

    in and out from form content

--- src/Resources/public/js/cmf_grapes.js
+++ src/Resources/public/js/cmf_grapes.js
@@ -3,26 +3,22 @@ GrapesJSAdapter = function (options) {
 		options.grapes_options = {};
 	}
 	this.grapesOptions = options.grapes_options;
-	if (!options.request.load) {
-		throw 'cmfGrapesjs needs an AJAX URL to lazy load the content, pass it using the `request.load` option.';
-	}
 	this.request = options.request;
 	this.rootNode = options.root_node;
+	this.formId = options.form_id;
+	this.formData = $('#'+this.formId).val();
+
 }
 
 GrapesJSAdapter.prototype.bindToElement = function ($element) {
 	this.grapesOptions.container = '#' + $element[0].id;
 	const editor = grapesjs.init(this.grapesOptions);
 	const domComponents = editor.DomComponents;
-	const url = this.request.load(this.rootNode).url;
-	jQuery.ajax(url, {
-		success: (response) => {
-			domComponents.addComponent(response.payload_type);
-		}
+	domComponents.clear();
+	domComponents.addComponent(this.formData)
+	editor.on('component:update', () => {
+		$('#'+this.formId).val(editor.getHtml());
 	});
-
-
-
 }
 
 /**
